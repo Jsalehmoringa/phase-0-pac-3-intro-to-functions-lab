5700ce94bbd45773473fb01cc01e46ed6655c5a0
jamil <jamil.saleh@student.moringaschool.com>
2022-11-22 15:27:50 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+function shout(string) {
+    return string.toUpperCase('hello');
+  }
+
+function whisper(string) {
+    return string.toLowerCase('hello');
+  }
+
+function logShout(string) {
+    console.log('HELLO');
+  }
+
+function logWhisper(string) {
+    console.log('hello');
+  }
+    
+function sayHiToGrandma(string) {
+    if (string === 'toLowerCase') {
+      console.log("I can't hear you!");
+      } else {
+      console.log("hello");
+      }
+
+      if (string === 'toUpperCase') {
+        console.log("YES INDEED!");
+        } else {
+        console.log("HELLO");
+        }
+
+        if (string === 'Let\'s have dinner together!') {
+          console.log("I would love to!");
+          } else {
+          console.log("Let's have dinner together!");
+          }
+      
+    }
